54367efa855d9a9ff40f274a3679002a34d73e82
Elton-Henrique-Oliveira <terezafe.oli@gmail.com>
2023-12-09 23:57:51 -0300

    ...

--- react-01/src/App.jsx
+++ react-01/src/App.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 
-import Data from "./components/Data";
-
 export default function App(){
     return(
         <>
             <h3>Hello Word!</h3>
-            <Data />
         </>
     )
 }
