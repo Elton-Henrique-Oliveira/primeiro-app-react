09ffb5220ff34a31e953772be893e188d64981db
Elton-Henrique-Oliveira <terezafe.oli@gmail.com>
2023-12-10 00:07:43 -0300

    exemplo de como utilizar props

--- react-01/src/App.jsx
+++ react-01/src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
+import Article from "./components/Article";
+
 export default function App(){
     return(
         <>
             <h3>Hello Word!</h3>
+            <Article titulo="Noticia um"/>
+            <Article titulo="Noticia dois"/>
         </>
     )
 }
---
+++ react-01/src/components/Article.jsx
@@ -0,0 +1,8 @@
+export default function Article(props){
+    return(
+        <>
+            <h3>{props.titulo}</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, quibusdam.</p>
+        </>
+    )
+}
