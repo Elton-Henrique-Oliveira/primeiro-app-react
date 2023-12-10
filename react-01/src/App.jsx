109a3dc0e17f3451fe12e5fe87e123ca811fff30
Elton-Henrique-Oliveira <terezafe.oli@gmail.com>
2023-12-10 00:23:56 -0300

    trabalhando com map de uma lista

--- react-01/src/App.jsx
+++ react-01/src/App.jsx
@@ -3,18 +3,22 @@ import React from "react";
 import Article from "./components/Article";
 
 export default function App(){
+
+    let artigos = [
+        { titulo: "Um", subtitulo: "Sub Um" },
+        { titulo: "Dois", subtitulo: "Sub Dois" },
+    ]
+
     return(
         <>
             <h3>Hello Word!</h3>
-            
-            <Article 
-                titulo="Noticia um" 
-                subtitulo="Subtitulo Noticia um"
-            />
-            <Article 
-                titulo="Noticia dois" 
-                subtitulo="Subtitulo Noticia dois"
-            />
+
+            {artigos.map((artigo, index) =>(
+                <Article 
+                    titulo={artigo.titulo}
+                    subtitulo={artigo.subtitulo}
+                />
+            ))}
         </>
     )
 }
--- react-01/src/components/Article.jsx
+++ react-01/src/components/Article.jsx
@@ -1,8 +1,8 @@
-export default function Article(props){
+export default function Article({titulo, subtitulo}){
     return(
         <>
-            <h3>{props.titulo}</h3>
-            <h5>{props.subtitulo}</h5>
+            <h3>{titulo}</h3>
+            <h5>{subtitulo}</h5>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, quibusdam.</p>
         </>
     )
