d2ebced85c9c7555575a42a791456080e5c6f062
Elton-Henrique-Oliveira <terezafe.oli@gmail.com>
2023-12-09 23:57:11 -0300

    arquivo css

--- react-01/src/App.jsx
+++ react-01/src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
-import Data from "./components/Data";
+import './css/App.css'
+
+import Dados from "./components/Dados";
 
 export default function App(){
     return(
         <>
-            <h3>Hello Word!</h3>
-            <Data />
+            <h3>Isso fica bem é com CSS!</h3>
+            <h5 className="cor-texto">Outro texto</h5>
+            <Dados />
         </>
     )
 }
